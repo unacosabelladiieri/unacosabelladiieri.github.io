e583c07dc3f7e4e19c33faed1152cb6847b49c4f
Vittorio <vdeiuliis@gmail.com>
2026-08-14 20:10:40 +0200

    Pagina Chi sono riscritta; il copyright si firma v.

--- src/site.config.ts
+++ src/site.config.ts
@@ -9,6 +9,8 @@ export const site = {
   // Sottotitolo mostrato in home e nei metadati
   description: 'Appunti su una cosa bella al giorno: libri, musica, schermo, viaggi e tutto il resto.',
   author: 'Vittorio',
+  /** Come firmi il copyright in fondo alle pagine. */
+  firma: 'v.',
   lang: 'it',
   /**
    * URL pubblico del sito.
